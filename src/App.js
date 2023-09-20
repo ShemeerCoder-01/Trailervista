5a7c569ab775924706ce46e2504495654e10037e
Shemeer <shameerbem@gmail.com>
2023-09-20 12:42:45 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import SignupPage from './components/SignUpPage';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Routes,Route } from 'react-router-dom';
 import { isSignedIn } from './actions/signInChecker';
 // import { auth } from './firebase';
@@ -11,18 +11,21 @@ import { useNavigate } from 'react-router-dom';
 function App() {
 
  const navigate = useNavigate();
+ const [user,setUser] = useState(false);
 
 
   useEffect(() => {
     // Add an observer to watch for changes in the user's authentication state
     const user = isSignedIn();
     if (user) {
+      setUser(true);
       navigate('/Home');
     } else {
+      setUser(false);
       navigate('/');
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+   
+  }, [navigate,user]);
 
   return (
     <div className="App">
