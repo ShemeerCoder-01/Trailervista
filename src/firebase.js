e65695f1d24bbb748bc4d44ad71d85435d897d6b
Shemeer <shameerbem@gmail.com>
2023-09-20 11:48:43 +0530

    updated firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
 
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
-console.log("domain",process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
